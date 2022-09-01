8b2ab3d7f291c4679c5c48f77e2272bc0001439a
hiromi-in <piromi.ishikawa@gmail.com>
2022-09-01 15:51:32 -0500

    Add jQuery.

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+//$(document).ready(function(){
+//    $(".show_tea_mocha").mouseover(function(){
+//      $(".teatime_mocha").removeClass('hidden');
+//    });
+//    $(".show_tea_mocha").mouseout(function(){
+//      $(".teatime_mocha").addClass('hidden');
+//    });
+//  });
+//  
+//
+
