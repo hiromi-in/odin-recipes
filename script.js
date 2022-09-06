97937db4ee3afa3cfbc1a1a74753692c65be3778
hiromi-in <piromi.ishikawa@gmail.com>
2022-09-06 11:32:43 -0500

    Add teatime when hover on images.

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 'use strict';
 
 
-//$(document).ready(function(){
-//    $(".show_tea_mocha").mouseover(function(){
-//      $(".teatime_mocha").removeClass('hidden');
-//    });
-//    $(".show_tea_mocha").mouseout(function(){
-//      $(".teatime_mocha").addClass('hidden');
-//    });
-//  });
-//  
+$(document).ready(function(){
+    $(".show_tea_mocha").mouseover(function(){
+      $(".teatime_mocha").removeClass('hidden');
+    });
+    $(".show_tea_mocha").mouseout(function(){
+      $(".teatime_mocha").addClass('hidden');
+    });
+  });
+  
+
+  $(document).ready(function(){
+    $(".show_tea_chai").mouseover(function(){
+      $(".teatime_chai").removeClass('hidden');
+    });
+    $(".show_tea_chai").mouseout(function(){
+      $(".teatime_chai").addClass('hidden');
+    });
+  });
+
+  $(document).ready(function(){
+    $(".show_tea_matcha").mouseover(function(){
+      $(".teatime_matcha").removeClass('hidden');
+    });
+    $(".show_tea_matcha").mouseout(function(){
+      $(".teatime_matcha").addClass('hidden');
+    });
+  });
+  
 //
 
 //logoの表示
